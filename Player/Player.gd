a8ba57428d0359e64c0f4965999047fbc2db8dbd
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-01-10 23:35:56 +0200

    Lesson 12

--- Player/Player.gd
+++ Player/Player.gd
@@ -12,6 +12,7 @@ enum {
 
 var state = STATE_MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 onready var animationTree = $AnimationTree
 onready var animationState = $AnimationTree.get("parameters/playback")
 
@@ -23,7 +24,7 @@ func _process(delta):
 		STATE_MOVE:
 			move_state(delta)
 		STATE_ROLL:
-			pass
+			roll_state(delta)
 		STATE_ATTACK:
 			attack_state(delta)
 
@@ -36,24 +37,41 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, \
 			ACCELERATION * delta)
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		animationState.travel("Idle")
 	
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = STATE_ATTACK
+	elif Input.is_action_just_pressed("roll"):
+		state = STATE_ROLL
 
+func roll_state(delta):
+	velocity = roll_vector * MAX_SPEED * 1.5
+	animationState.travel("Roll")
+	move()
+	
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
 func attack_done():
 	state = STATE_MOVE
+
+func roll_done():
+	velocity = velocity * 0.8
+	state = STATE_MOVE
+
+func move():
+	velocity = move_and_slide(velocity)
+	
