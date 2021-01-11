29c97c3dd9b66062341a3f5af5e47d58f5431547
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-01-11 12:39:12 +0200

    Lesson 15

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 onready var stats = $Stats
+const deathEffectScene = preload("res://Effects/EnemyDeathEffect.tscn")
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -11,6 +12,8 @@ func _on_Area2D_area_entered(area):
 	knockback = area.knockback_vector * 120
 	stats.health -= area.damage
 
-
 func _on_Stats_no_health():
+	var deathEffect = deathEffectScene.instance()
+	get_parent().add_child(deathEffect)
+	deathEffect.global_position = global_position
 	queue_free()
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
+const grassEffectScene = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
-	var grassEffectScene = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = grassEffectScene.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
-
-
 
 func _on_Area2D_area_entered(area):
 	create_grass_effect()
